43fd89e81e29ff5d0bcaa22a086e30dd4e8a7f63
cnpoe <cnpoek@gmail.com>
2019-08-01 12:49:00 -0500

    delete, create, update

--- src/app/students.component.ts
+++ src/app/students.component.ts
@@ -11,6 +11,33 @@ import { Student } from './student';
 export class StudentsComponent  {
   students = StudentsDataService.generate(100);
   student = new Student();
+  showForm:boolean = false;
+
+  onChange(value: string) {
+    if (value) {
+      this.students.sort((a, b) => {
+        if (a[value] > b[value]) {
+          return 1;
+        }
+        if (a[value] < b[value]) {
+          return -1;
+        }
+        return 0;
+      })
+    }
+  }
+
+  openForm() {
+    this.showForm = true;
+  }
+
+  addStudent() {
+    this.student.therapies = this.student.therapies.split(',');
+    this.students.push(this.student);
+    this.student = new Student();
+    console.log(this.student);
+    this.showForm = false;
+  }
 
   updateList(id: number, property: string, event: any) {
     const editField = event.target.textContent;
@@ -24,5 +51,10 @@ export class StudentsComponent  {
 
   changeValue(id: number, property: string, event: any) {
     this.student = event.target.textContent;
+    this.student = new Student();
+  }
+
+  deleteElement(id: number, event: any) {
+    this.students.splice(id, 1);
   }
 }
